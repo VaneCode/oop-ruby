c142eb687eae2df8362179a8b3a323bb16c993e3
vanessaBonito <vanebonito@hotmail.com>
2022-10-03 13:51:53 -0500

    Set foods in class Animal

--- animal.rb
+++ animal.rb
@@ -1,10 +1,12 @@
 require './remover'
+require './foods'
 class Animal
   def initialize(type, number_of_legs, name = 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
     @type = type
+    @liked_food = NoFood.new()
   end
 
   # Shortcut of getter method
