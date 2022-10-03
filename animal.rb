45cceba68872f96f0b07398b4e69dae233af0ac7
vanessaBonito <vanebonito@hotmail.com>
2022-10-03 13:53:19 -0500

    Add method likes_food using @liked_food in Animal

--- animal.rb
+++ animal.rb
@@ -6,7 +6,7 @@ class Animal
     @name = name
     @number_of_legs = number_of_legs
     @type = type
-    @liked_food = NoFood.new()
+    @liked_food = NoFood.new
   end
 
   # Shortcut of getter method
@@ -33,5 +33,9 @@ class Animal
   def remove_leg
     remover = Remover.new
     @number_of_legs = remover.decrease(@number_of_legs)
+  end
+
+  def likes_food?(food)
+    @liked_food.is_liked?(food)
   end
 end
