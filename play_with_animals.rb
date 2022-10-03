f6344f6a91c5472f09586ec7d7ed73189eeca7a2
vanessaBonito <vanebonito@hotmail.com>
2022-10-03 12:53:35 -0500

    Try method remove_leg in play_with_animals

--- play_with_animals.rb
+++ play_with_animals.rb
@@ -6,6 +6,15 @@ animal = Animal.new('lion', 4, 'Rex')
 dog = Dog.new('black', 'Rex')
 spider = Spider.new(85, 'Wilma')
 
-p animal
-p dog
-p spider
+p animal.number_of_legs
+p dog.number_of_legs
+p spider.number_of_legs
+
+animal.remove_leg()
+dog.remove_leg()
+spider.remove_leg()
+
+p animal.number_of_legs
+p dog.number_of_legs
+p spider.number_of_legs
+
