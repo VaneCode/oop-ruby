92091658dec9a4c145ddf950198d56d4000d67c1
vanessaBonito <vanebonito@hotmail.com>
2022-10-04 14:35:08 -0500

    Try to add an animal to an owner and vice versa

--- play_with_animals.rb
+++ play_with_animals.rb
@@ -7,49 +7,60 @@ animal = Animal.new('lion', 4, 'Rex')
 dog = Dog.new('black', 'Rex')
 spider = Spider.new(85, 'Wilma')
 
-# Try remove leg
-p animal.number_of_legs
-p dog.number_of_legs
-p spider.number_of_legs
-
-animal.remove_leg
-dog.remove_leg
-spider.remove_leg
-
-p animal.number_of_legs
-p dog.number_of_legs
-p spider.number_of_legs
-
-# Try likes food
-
-p animal.likes_food?('meat')
-p dog.likes_food?('meat')
-p spider.likes_food?('meat')
-
-p animal.likes_food?('bug')
-p dog.likes_food?('bug')
-p spider.likes_food?('bug')
-
 # Try have an owner and give it animals.
 
 alex = Owner.new('Alex')
-alex.animals
+# alex.animals
+# alex.add_animal(dog)
+# p alex.animals
+# alex.add_animal(spider)
+# p alex.animals
+# alex.add_animal(animal)
+# p alex.animals.map(&:name)
+
+# p alex.animals.count
+# p alex.animals.first.name
+# p alex.animals.first.number_of_legs
+
+# Try add owner to an animal
+# p alex.animals.last.owner.name
+# p animal.owner
+# animal.owner = alex
+# p animal.owner
+# p animal.owner.name
+
+# p alex.animals.last.owner.name
+
+# Try to add an animal to an owner the owner of the animal is set as well, and vice versa.
+
+p dog.owner
 alex.add_animal(dog)
+p dog.owner
+p dog.owner.name
 p alex.animals
+
+p spider.owner
 alex.add_animal(spider)
+p spider.owner
+p spider.owner.name
 p alex.animals
+
+p animal.owner
 alex.add_animal(animal)
-p alex.animals.map(&:name)
+p animal.owner
+p animal.owner.name
 
 p alex.animals.count
 p alex.animals.first.name
 p alex.animals.first.number_of_legs
 
-# Try add owner to an animal
-# p alex.animals.last.owner.name
-# p animal.owner
-animal.owner = alex
-p animal.owner
-p animal.owner.name
+second_animal = Animal.new('cat', 4, 'Kitty')
+second_animal.owner
+alex.animals.count
 
-p alex.animals.last.owner.name
+second_animal.owner = alex
+
+second_animal.owner
+alex.animals.count
+alex.animals.last
+alex.animals.last.name
