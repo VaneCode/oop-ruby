17418266abcd97175ddcd4d7f38cb261f17037e3
vanessaBonito <vanebonito@hotmail.com>
2022-10-04 14:13:04 -0500

    Create owner and give it animals

--- play_with_animals.rb
+++ play_with_animals.rb
@@ -1,6 +1,7 @@
 require './animal'
 require './dog'
 require './spider'
+require './owner'
 
 animal = Animal.new('lion', 4, 'Rex')
 dog = Dog.new('black', 'Rex')
@@ -28,3 +29,18 @@ p spider.likes_food?('meat')
 p animal.likes_food?('bug')
 p dog.likes_food?('bug')
 p spider.likes_food?('bug')
+
+# Try have an owner and give it animals.
+
+alex = Owner.new('Alex')
+alex.animals
+alex.add_animal(dog)
+p alex.animals
+alex.add_animal(spider)
+p alex.animals
+alex.add_animal(animal)
+p alex.animals.map(&:name)
+
+p alex.animals.count
+p alex.animals.first.name
+p alex.animals.first.number_of_legs
