fd2d7465ee999112053765095cad0fa64aad085a
vanessaBonito <vanebonito@hotmail.com>
2022-10-06 11:22:40 -0500

    Fix linters errors

--- play_with_animals.rb
+++ play_with_animals.rb
@@ -73,21 +73,24 @@ spider = Spider.new(85, 'Bob')
 vet_maria = Vet.new('Maria', 'New York')
 vet_john = Vet.new('John', 'San Francisco')
 
-p visit_1 = Visit.new('2017-12-22', dog, vet_maria)
-p visit_2 = Visit.new('2017-12-31', dog, vet_maria)
+visit1 = Visit.new('2017-12-22', dog, vet_maria)
+visit2 = Visit.new('2017-12-31', dog, vet_maria)
+visit3 = Visit.new('2017-11-11', spider, vet_john)
+visit4 = Visit.new('2017-10-10', spider, vet_maria)
+
+p visit1
+p visit2
+p visit3
+p visit4
 
 p dog.visits.count
 p dog.visits.map(&:date)
 p vet_john.visits.count
 p vet_maria.visits.count
-p vet_maria.visits.map { |visit| visit.animal.name }
-
-visit_3 = Visit.new('2017-11-11', spider, vet_john)
-visit_4 = Visit.new('2017-10-10', spider, vet_maria)
-
+p(vet_maria.visits.map { |visit| visit.animal.name })
 p spider.visits.count
 p spider.visits.map(&:date)
 p vet_john.visits.count
-p vet_john.visits.map { |visit| visit.animal.name }
+p(vet_john.visits.map { |visit| visit.animal.name })
 p vet_maria.visits.count
-p vet_maria.visits.map { |visit| visit.animal.name }
+p(vet_maria.visits.map { |visit| visit.animal.name })
